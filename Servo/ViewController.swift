c397b24fb23d47d592d33067d339f3424302944d
Oguzhan Demirci <sivas_1905.58@hotmail.com>
2018-05-06 16:57:05 +0300

    Initial Commit

---
+++ Servo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Servo
+//
+//  Created by oğuzhan on 6.05.2018.
+//  Copyright © 2018 oguzhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
